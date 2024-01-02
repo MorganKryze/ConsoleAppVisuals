0c80c922111ca8c0dcc39ac45c1a2361a342bb84
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-01-02 16:32:04 +0100

    🚧 (refactoring) clean code

--- src/ConsoleAppVisuals/Sidebar.cs
+++ src/ConsoleAppVisuals/Sidebar.cs
@@ -7,12 +7,14 @@ namespace ConsoleAppVisuals;
 /// <summary>
 ///    A class that represents a sidebar on the console.
 /// </summary>
-public class Sidebar { 
+public class Sidebar
+{
     /// <summary>
     ///   Creates a new instance of the <see cref="Sidebar"/> class.
     /// </summary>
     /// <exception cref="NotImplementedException">Feature not implemented yet.</exception>
-    public Sidebar() { 
+    public Sidebar()
+    {
         throw new NotImplementedException();
     }
 }
