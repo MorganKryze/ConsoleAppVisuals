e926141cb6f9c840da7174d0a410086d5ef49a77
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-01-03 19:02:31 +0100

    :refactor (enum) add up and down direction

---
+++ src/ConsoleAppVisuals/models/Direction.cs
@@ -0,0 +1,20 @@
+/*
+    MIT License 2023 MorganKryze
+    For full license information, please visit: https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/LICENSE
+*/
+namespace ConsoleAppVisuals;
+
+/// <summary>
+/// The <see cref="Direction"/> enum defines the direction in some space.
+/// </summary>
+public enum Direction
+{
+    /// <summary>
+    /// The up direction.
+    /// </summary>
+    Up,
+    /// <summary>
+    /// The down direction.
+    /// </summary>
+    Down
+}
