09adf1a0c43480d1384c4eb4bffa9f2503c13fc6
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-02-15 13:20:47 +0000

    🌟 add compatibility to net8.0

---
+++ testing/elements/interactive/UnitTestEmbeddedText.cs
@@ -0,0 +1,8 @@
+/*
+    GNU GPL License 2024 MorganKryze(Yann Vidamment)
+    For full license information, please visit: https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/LICENSE
+*/
+namespace ConsoleAppVisuals;
+
+[TestClass]
+public class UnitTestEmbeddedText { }
