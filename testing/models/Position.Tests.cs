5cd2780a367a6b9f74eb436cd2ed0cf0d546cdfd
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-02-16 22:20:39 +0000

    ✅ clean models

--- testing/models/Position.Tests.cs
+++ testing/models/Position.Tests.cs
@@ -8,64 +8,87 @@ namespace testing;
 public class UnitTestPosition
 {
     [TestMethod]
-    public void TestConstructor()
+    public void Constructor_PositionInitializedWithGivenValues()
     {
+        // Arrange
         var position = new Position(1, 2);
+
+        // Act & Assert
         Assert.AreEqual(1, position.X);
         Assert.AreEqual(2, position.Y);
     }
 
     [TestMethod]
-    public void TestCopyConstructor()
+    public void CopyConstructor_PositionCopiedCorrectly()
     {
+        // Arrange
         var original = new Position(1, 2);
+
+        // Act
         var copy = new Position(original);
+
+        // Assert
         Assert.AreEqual(original, copy);
     }
 
     [TestMethod]
-    public void TestToString()
+    public void ToString_ReturnsCorrectStringRepresentation()
     {
+        // Arrange
         var position = new Position(1, 2);
+
+        // Act & Assert
         Assert.AreEqual("Line : 1 ; Column : 2", position.ToString());
     }
 
     [TestMethod]
-    public void TestEquals()
+    public void Equals_TwoPositionsEqualWhenCoordinatesMatch()
     {
+        // Arrange
         var position1 = new Position(1, 2);
         var position2 = new Position(1, 2);
         var position3 = new Position(2, 1);
+
+        // Act & Assert
         Assert.IsTrue(position1.Equals(position2));
         Assert.IsFalse(position1.Equals(position3));
     }
 
     [TestMethod]
-    public void TestGetHashCode()
+    public void GetHashCode_ReturnsSameHashCodeForEqualPositions()
     {
+        // Arrange
         var position1 = new Position(1, 2);
         var position2 = new Position(1, 2);
         var position3 = new Position(2, 1);
+
+        // Act & Assert
         Assert.AreEqual(position1.GetHashCode(), position2.GetHashCode());
         Assert.AreNotEqual(position1.GetHashCode(), position3.GetHashCode());
     }
 
     [TestMethod]
-    public void TestOperatorEquals()
+    public void OperatorEquals_ReturnsTrueWhenPositionsAreEqual()
     {
+        // Arrange
         var position1 = new Position(1, 2);
         var position2 = new Position(1, 2);
         var position3 = new Position(2, 1);
+
+        // Act & Assert
         Assert.IsTrue(position1 == position2);
         Assert.IsFalse(position1 == position3);
     }
 
     [TestMethod]
-    public void TestOperatorNotEquals()
+    public void OperatorNotEquals_ReturnsTrueWhenPositionsAreNotEqual()
     {
+        // Arrange
         var position1 = new Position(1, 2);
         var position2 = new Position(1, 2);
         var position3 = new Position(2, 1);
+
+        // Act & Assert
         Assert.IsFalse(position1 != position2);
         Assert.IsTrue(position1 != position3);
     }
