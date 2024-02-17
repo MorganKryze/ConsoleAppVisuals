28f46f7478353a26e9258f1b6f50f773baca083a
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-02-17 12:10:12 +0000

    🚑 fix issue with width of loading bars

--- src/ConsoleAppVisuals/elements/LoadingBar.cs
+++ src/ConsoleAppVisuals/elements/LoadingBar.cs
@@ -97,7 +97,12 @@ public class LoadingBar : Element
         int additionalDuration = 1000
     )
     {
-        _text = text[..Math.Min(text.Length, Console.WindowWidth - 1)];
+        if(Console.WindowWidth -1 < 0){
+            _text = text[..Math.Min(text.Length, 0)];
+        }
+        else{
+            _text = text[..Math.Min(text.Length, Console.WindowWidth - 1)];
+        }
         _progress = progress;
         _placement = placement;
         _line = Window.CheckLine(line) ?? Window.GetLineAvailable(placement);
