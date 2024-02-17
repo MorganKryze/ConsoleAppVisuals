db46f7013a3efb61f57af7a051634da53dd66373
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-02-17 13:14:09 +0000

    ✅ 100% on FloatSelector

---
+++ testing/elements/interactive/ScrollingMenu.Tests.cs
@@ -0,0 +1,20 @@
+/*
+    GNU GPL License 2024 MorganKryze(Yann Vidamment)
+    For full license information, please visit: https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/LICENSE
+*/
+namespace ConsoleAppVisuals;
+
+[TestClass]
+public class UnitTestScrollingMenu
+{
+    #region Cleanup
+    [TestCleanup]
+    public void Cleanup()
+    {
+        // Cleanup
+        Window.RemoveAllElements();
+    }
+    #endregion
+
+    
+}
