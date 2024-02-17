b95140b66a9270683367842cbff78ffdc3edf6bc
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-02-17 13:22:17 +0000

    ✅ 100% on ScrollingMenu

--- testing/elements/interactive/ScrollingMenu.Tests.cs
+++ testing/elements/interactive/ScrollingMenu.Tests.cs
@@ -16,5 +16,132 @@ public class UnitTestScrollingMenu
     }
     #endregion
 
-    
+    #region Placement
+    [TestMethod]
+    [TestCategory("ScrollingMenu")]
+    public void Placement_Getter()
+    {
+        // Arrange
+        var scrollingMenu = new ScrollingMenu("Question",0, Placement.TopCenter, 0, "Choice1", "Choice2", "Choice3");
+
+        // Act
+        var placement = scrollingMenu.Placement;
+
+        // Assert
+        Assert.AreEqual(Placement.TopCenter, placement);
+    }
+    #endregion
+
+    #region Line
+    [TestMethod]
+    [TestCategory("ScrollingMenu")]
+    public void Line_Getter()
+    {
+        // Arrange
+        var scrollingMenu = new ScrollingMenu("Question", 0, Placement.TopCenter, 0, "Choice1", "Choice2", "Choice3");
+
+        // Act
+        var line = scrollingMenu.Line;
+
+        // Assert
+        Assert.AreEqual(0, line);
+    }
+
+    [TestMethod]
+    [TestCategory("ScrollingMenu")]
+    public void Line_Getter_NoInput()
+    {
+        // Arrange
+        var scrollingMenu = new ScrollingMenu("Question", 0, Placement.TopCenter, default, "Choice1", "Choice2", "Choice3");
+
+        // Act
+        var line = scrollingMenu.Line;
+
+        // Assert
+        Assert.AreEqual(0, line);
+    }
+    #endregion
+
+    #region Height
+    [TestMethod]
+    [TestCategory("ScrollingMenu")]
+    public void Height_Getter()
+    {
+        // Arrange
+        var scrollingMenu = new ScrollingMenu("Question", 0, Placement.TopCenter, 0, "Choice1", "Choice2", "Choice3");
+
+        // Act
+        var height = scrollingMenu.Height;
+
+        // Assert
+        Assert.AreEqual(5, height);
+    }
+    #endregion
+
+    #region Width
+    [TestMethod]
+    [TestCategory("ScrollingMenu")]
+    public void Width_Getter()
+    {
+        // Arrange
+        var scrollingMenu = new ScrollingMenu("Question", 0, Placement.TopCenter, 0, "Choice1", "Choice2", "Choice3");
+
+        // Act
+        var width = scrollingMenu.Width;
+
+        // Assert
+        Assert.AreEqual(11, width);
+    }
+    #endregion
+
+    #region Question
+    [TestMethod]
+    [TestCategory("ScrollingMenu")]
+    public void Question_Getter()
+    {
+        // Arrange
+        var scrollingMenu = new ScrollingMenu("Question", 0, Placement.TopCenter, 0, "Choice1", "Choice2", "Choice3");
+
+        // Act
+        var question = scrollingMenu.Question;
+
+        // Assert
+        Assert.AreEqual("Question", question);
+    }
+    #endregion
+
+    #region Choices
+    [TestMethod]
+    [TestCategory("ScrollingMenu")]
+    public void Choices_Getter()
+    {
+        // Arrange
+        var scrollingMenu = new ScrollingMenu("Question", 0, Placement.TopCenter, 0, "Choice1", "Choice2", "Choice3");
+
+        // Act
+        var choices = scrollingMenu.Choices;
+
+        // Assert
+        Assert.AreEqual(3, choices.Length);
+        Assert.AreEqual("Choice1", choices[0]);
+        Assert.AreEqual("Choice2", choices[1]);
+        Assert.AreEqual("Choice3", choices[2]);
+    }
+    #endregion
+
+    #region DefaultIndex
+    [TestMethod]
+    [TestCategory("ScrollingMenu")]
+    public void DefaultIndex_Getter()
+    {
+        // Arrange
+        var scrollingMenu = new ScrollingMenu("Question", 1, Placement.TopCenter, 0, "Choice1", "Choice2", "Choice3");
+
+        // Act
+        var defaultIndex = scrollingMenu.DefaultIndex;
+
+        // Assert
+        Assert.AreEqual(1, defaultIndex);
+    }
+    #endregion
 }
