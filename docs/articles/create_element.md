e87c8e1d9355377f35116d53ee6162004470b003
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-02-21 16:51:50 +0000

    📖 try new feature embedded code

--- docs/articles/create_element.md
+++ docs/articles/create_element.md
@@ -1,6 +1,81 @@
 # Create custom visual elements
 
-Work in progress...
+## Introduction
 
-> [!NOTE]
-> This subject is briefly tackled in the docs but if this part really thrills you, feel free to notify me by [opening an issue](https://github.com/MorganKryze/ConsoleAppVisuals/issues) or [contact me by email](mailto:morgan@kodelab.fr).
+This article will guide you through the process of creating custom visual elements using the library. This will enable you to create static elements as well as interactive elements that can be used in your applications.
+
+## Prerequisites
+
+- .NET framework 6 or later
+- ConsoleAppVisuals library: 3.0.0 or later
+- Having looked at the project from the [Introduction section](/introduction/index.html)
+
+Here is the file structure of the project:
+
+```bash
+Example_project  <-- root
+└───MyApp
+    ├───bin
+    ├───MyApp.csproj
+    └───Program.cs
+```
+
+## Static element
+
+Static elements are visual elements that do not have any interactive behavior. They are used to display information to the user. They can be updated and change display properties.
+
+### Setup
+
+Start by creating a new file in your project and name it `StaticExample.cs`. Then, add the following code to the file (see real example in the [example project](https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/)):
+
+```csharp
+using ConsoleAppVisuals;
+
+namespace MyApp
+{
+    // This object is a slight modification of the EmbeddedText object for the demo (here not interactive).
+    public class StaticExample : Element
+    {
+        #region Fields
+        // Add your custom fields here.
+        #endregion
+
+        #region Properties
+        // Add overridden properties here.
+        // You may also add your custom properties here.
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// The natural constructor of the StaticExample element.
+        /// </summary>
+        public StaticExample(){}
+        #endregion
+
+        #region Methods
+        // Add your custom methods here.
+        #endregion
+
+        #region Rendering
+        /// <summary>
+        /// Renders the StaticExample element.
+        /// </summary>
+        protected override void RenderElementActions()
+        {
+            // This method is mandatory to render correctly your element. If not, an error will be thrown.
+            // Add what the display code here.
+        }
+        #endregion
+    }
+}
+```
+
+### Customization
+
+Now let's look at the `Element` class. This class is the base class for all visual elements. It contains all the properties and methods that are necessary for the rendering of the elements. You can override some of these properties and methods to customize the behavior of your element.
+
+[!code-sharp[](assets/code/Element.cs?highlight=35,40,45,51,57,63,69,129,138,144,179)]
+
+## Interactive element
+
+## Visualize all elements available
---
+++ docs/assets/code/Element.cs
@@ -0,0 +1,198 @@
+/*
+    GNU GPL License 2024 MorganKryze(Yann Vidamment)
+    For full license information, please visit: https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/LICENSE
+*/
+namespace ConsoleAppVisuals;
+
+/// <summary>
+/// Defines the basic properties of an console element.
+/// </summary>
+/// <remarks>
+/// For more information, refer to the following resources:
+/// <list type="bullet">
+/// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+/// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/Program.cs">Example Project</a></description></item>
+/// </list>
+/// </remarks>
+public abstract class Element
+{
+    #region Properties
+    /// <summary>
+    /// The id number of the element.
+    /// </summary>
+    /// <remarks>This property is sealed. The ID of an element is automatically generated and managed by the <see cref="Window"/> class.</remarks>
+    public int Id { get; set; } = Window.NextId;
+
+    /// <summary>
+    /// The visibility of the element.
+    /// </summary>
+    /// <remarks>This property is sealed. The visibility of an element is managed by the <see cref="ToggleVisibility"/> method.</remarks>
+    public bool Visibility { get; private set; } = Window.DEFAULT_VISIBILITY;
+
+    /// <summary>
+    /// The placement of the element.
+    /// </summary>
+    public virtual Placement Placement { get; set; } = Placement.TopCenter;
+
+    /// <summary>
+    /// The text alignment of the text of the element.
+    /// </summary>
+    public virtual TextAlignment TextAlignment { get; set; } = TextAlignment.Center;
+
+    /// <summary>
+    /// Whether the element is executable or not.
+    /// </summary>
+    public virtual bool IsInteractive { get; } = false;
+
+    /// <summary>
+    /// The line of the element in the console.
+    /// </summary>
+    /// <remarks>This property is marked as virtual. It is recommended to override this property in derived classes to make it more specific.</remarks>
+    public virtual int Line { get; } = 0;
+
+    /// <summary>
+    /// The height of the element.
+    /// </summary>
+    /// <remarks>This property is marked as virtual. It is recommended to override this property in derived classes to make it more specific.</remarks>
+    public virtual int Height { get; } = 0;
+
+    /// <summary>
+    /// The width of the element.
+    /// </summary>
+    /// <remarks>This property is marked as virtual. It is recommended to override this property in derived classes to make it more specific.</remarks>
+    public virtual int Width { get; } = 0;
+
+    /// <summary>
+    /// The maximum number of this element that can be drawn on the console.
+    /// </summary>
+    /// <remarks>This property is marked as virtual. It is recommended to override this property in derived classes to make it more specific.</remarks>
+    public virtual int MaxNumberOfThisElement { get; } = 1;
+
+    /// <summary>
+    /// The source of the element. This property is used to determine whether the element is a user element or a library element for internal use.
+    /// </summary>
+    /// <remarks><c>[ DANGEROUS ]</c> Please do not change this property unless you know what you are doing.</remarks>
+    public Source ElementSource { get; set; } = Source.User;
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// This method is used to toggle the visibility of the element.This method is used to toggle the visibility of the element. If the maximum number of this element is reached, an exception is thrown.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Thrown when the maximum number of this element is reached.</exception>
+    /// <remarks>This method is effectively sealed. The only way to change the visibility of an element is to use this method.</remarks>
+    public void ToggleVisibility()
+    {
+        if (Visibility)
+        {
+            Visibility = false;
+        }
+        else if (Window.AllowVisibilityToggle(Id))
+        {
+            Visibility = true;
+        }
+        else
+        {
+            throw new InvalidOperationException(
+                $"Operation not allowed, too many elements of {GetType()} already toggled from the maximum of {MaxNumberOfThisElement}. Consider turning off one element of this type."
+            );
+        }
+    }
+
+    /// <summary>
+    /// This method is used to draw the element on the console.
+    /// </summary>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/Program.cs">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    [Visual]
+    public void RenderElement()
+    {
+        if (Visibility)
+        {
+            RenderOptionsBeforeHand();
+            RenderElementActions();
+            RenderOptionsAfterHand();
+            
+        }
+    }
+
+    /// <summary>
+    /// This method is used to define the actions to perform when the element is drawn on the console.
+    /// </summary>
+    /// <remarks>This method is marked as virtual. It is recommended to override this method in derived classes to make it more specific.</remarks>
+    [Visual]
+    protected virtual void RenderElementActions()
+    {
+        throw new NotImplementedException("Consider overriding this method in the derived class.");
+    }
+
+    /// <summary>
+    /// This method is used to set options before drawing the element on the console.
+    /// </summary>
+    [Visual]
+    protected virtual void RenderOptionsBeforeHand() { }
+
+    /// <summary>
+    /// This method is used to set options after drawing the element on the console.
+    /// </summary>
+    [Visual]
+    protected virtual void RenderOptionsAfterHand() { }
+
+    /// <summary>
+    /// This method is used to draw the space taken by the element on the console.
+    /// </summary>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/Program.cs">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    [Visual()]
+    public void RenderElementSpace()
+    {
+        if (Visibility)
+        {
+            Core.SaveColorPanel();
+            Core.SetForegroundColor(Core.GetRandomColor());
+            Core.WriteMultiplePositionedLines(
+                false,
+                Placement.ToTextAlignment(),
+                true,
+                Line,
+                GetRenderSpace()
+            );
+            Core.LoadSavedColorPanel();
+        }
+    }
+
+    /// <summary>
+    /// This method is used to simulate the drawing space of the element on the console.
+    /// </summary>
+    /// <returns>The space taken by the element.</returns>
+    /// <remarks>This method is marked as virtual. It is recommended to override this method in derived classes to make it more specific.</remarks>
+    protected virtual string[] GetRenderSpace()
+    {
+        var space = new string[Height];
+        for (int i = 0; i < space.Length; i++)
+        {
+            space[i] = new string(' ', Width);
+        }
+        return space;
+    }
+
+    /// <summary>
+    /// This method is used to clear the space taken by the element on the console.
+    /// </summary>
+    [Visual]
+    public void Clear()
+    {
+        Core.WriteMultiplePositionedLines(false, TextAlignment, false, Line, GetRenderSpace());
+    }
+    #endregion
+}
