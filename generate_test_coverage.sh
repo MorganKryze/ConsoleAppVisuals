29ea484ddb5271872864dcc18b97eb9e90a5ae3f
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-03-05 19:05:23 +0000

    📖 rename and format generate test

--- generate_test_coverage.sh
+++ generate_test_coverage.sh
@@ -1,4 +1,7 @@
-dotnet build -c Release 
-dotnet test -c Release --no-build
+echo " --- Running tests --- \n"
+
+dotnet test -c Release
 coverlet testing/bin/Release/net8.0/testing.dll --target dotnet --targetargs "test -c Release --no-build" --format opencover --exclude-by-attribute Visual
 reportgenerator "-reports:./coverage.opencover.xml" "-targetdir:coverage" "-reporttypes:lcov"
+
+echo "\n --- Tests completed --- "
