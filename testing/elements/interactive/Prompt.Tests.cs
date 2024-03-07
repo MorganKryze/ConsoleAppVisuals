8abc183e656c2af0d0eb1843c4b802911b3c8faa
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-03-07 19:58:04 +0000

    ✅ (prompt) 100%

--- testing/elements/interactive/Prompt.Tests.cs
+++ testing/elements/interactive/Prompt.Tests.cs
@@ -32,6 +32,191 @@ public class UnitTestPrompt
         Assert.AreEqual(expected, actual);
     }
 
+    [TestMethod]
+    [TestCategory("Prompt")]
+    public void Placement_Getter()
+    {
+        // Arrange
+        var prompt = new Prompt("What is your name?", "John Doe", Placement.TopCenter, 20);
+
+        // Act
+        var actual = prompt.Placement;
+        var expected = Placement.TopCenter;
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    [TestCategory("Prompt")]
+    public void Height_Getter()
+    {
+        // Arrange
+        var prompt = new Prompt("What is your name?", "John Doe");
+
+        // Act
+        var actual = prompt.Height;
+        var expected = 3;
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    [TestCategory("Prompt")]
+    public void Width_Getter()
+    {
+        // Arrange
+        var prompt = new Prompt("What is your name?", "John Doe");
+
+        // Act
+        var actual = prompt.Width;
+        var expected = 19;
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    [TestCategory("Prompt")]
+    public void DefaultValue_Getter()
+    {
+        // Arrange
+        var prompt = new Prompt("What is your name?", "John Doe");
+
+        // Act
+        var actual = prompt.DefaultValue;
+        var expected = "John Doe";
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    [TestCategory("Prompt")]
+    public void MaxLength_Getter()
+    {
+        // Arrange
+        var prompt = new Prompt("What is your name?", "John Doe", Placement.TopCenter, 20);
+ 
+        // Act
+        var actual = prompt.MaxLength;
+        var expected = 20;
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    [TestCategory("Prompt")]
+    public void MaxLength_Getter_DefaultValue()
+    {
+        // Arrange
+        var prompt = new Prompt("What is your name?", "John Doe");
+
+        // Act
+        var actual = prompt.MaxLength;
+        var expected = 10;
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    #endregion
+
+    #region UpdateQuestion
+
+    [TestMethod]
+    [TestCategory("Prompt")]
+    public void UpdateQuestion()
+    {
+        // Arrange
+        var prompt = new Prompt("What is your name?", "John Doe");
+
+        // Act
+        prompt.UpdateQuestion("What is your age?");
+        var actual = prompt.Question;
+        var expected = "What is your age?";
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    #endregion
+
+    #region UpdateDefaultValue
+
+    [TestMethod]
+    [TestCategory("Prompt")]
+    public void UpdateDefaultValue()
+    {
+        // Arrange
+        var prompt = new Prompt("What is your name?", "John Doe");
+
+        // Act
+        prompt.UpdateDefaultValue("Jane Doe");
+        var actual = prompt.DefaultValue;
+        var expected = "Jane Doe";
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    #endregion
+
+    #region UpdateMaxLength
+
+    [TestMethod]
+    [TestCategory("Prompt")]
+    public void UpdateMaxLength()
+    {
+        // Arrange
+        var prompt = new Prompt("What is your name?", "John Doe");
+
+        // Act
+        prompt.UpdateMaxLength(20);
+        var actual = prompt.MaxLength;
+        var expected = 20;
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    [TestCategory("Prompt")]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    [DataRow(-1)]
+    [DataRow(300)]
+    public void UpdateMaxLength_OutsideRange(int maxLength)
+    {
+        // Arrange
+        var prompt = new Prompt("What is your name?", "John Doe");
+
+        // Act
+        prompt.UpdateMaxLength(maxLength);
+        
+    }
+
+    #endregion
+
+    #region UpdatePlacement
+
+    [TestMethod]
+    [TestCategory("Prompt")]
+    public void UpdatePlacement()
+    {
+        // Arrange
+        var prompt = new Prompt("What is your name?", "John Doe");
+
+        // Act
+        prompt.UpdatePlacement(Placement.TopRight);
+        var actual = prompt.Placement;
+        var expected = Placement.TopRight;
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
     #endregion
 
 }
