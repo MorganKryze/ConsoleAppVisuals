fae9fdce352f7c0d9f0da772a931b1f793086a4d
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-03-07 20:27:17 +0000

    🚑 last try

--- testing/elements/interactive/Prompt.Tests.cs
+++ testing/elements/interactive/Prompt.Tests.cs
@@ -186,7 +186,6 @@ public class UnitTestPrompt
     [TestCategory("Prompt")]
     [ExpectedException(typeof(ArgumentOutOfRangeException))]
     [DataRow(-1)]
-    [DataRow(1500)]
     public void UpdateMaxLength_OutsideRange(int maxLength)
     {
         // Arrange
