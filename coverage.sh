9431956e32dc9ee275f88873147dd46a2ec38e3c
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-03-14 22:19:25 +0000

    ✅ cleaner coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo " --- Running tests --- \n"
+echo "\n --- Building project --- \n"
+dotnet build -c Release --verbosity quiet
 
-dotnet test -c Release --verbosity m
-coverlet testing/bin/Release/net8.0/testing.dll --target dotnet --targetargs "test -c Release --no-build" --format opencover --exclude-by-attribute Visual
+echo "\n --- Generating Tests --- \n"
+coverlet testing/bin/Release/net8.0/testing.dll --target dotnet --targetargs "test -c Release --no-build --verbosity minimal" --format opencover --exclude-by-attribute Visual
+
+echo "\n --- Generating Coverage report --- \n"
 reportgenerator "-reports:./coverage.opencover.xml" "-targetdir:coverage" "-reporttypes:lcov"
-
-echo "\n --- Tests completed --- "
