e87cc6b37ee280b34343edbe89c081a2b14147a6
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-03-16 16:55:04 +0000

    🌟 add new element HeightSpacer

---
+++ testing/elements/passive/HeightSpacer.Tests.cs
@@ -0,0 +1,116 @@
+/*
+    GNU GPL License 2024 MorganKryze(Yann Vidamment)
+    For full license information, please visit: https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/LICENSE
+*/
+namespace testing;
+
+[TestClass]
+public class UnitTestSpacer
+{
+    #region Cleanup
+    [TestCleanup]
+    public void Cleanup()
+    {
+        // Cleanup
+        Window.RemoveAllElements();
+    }
+    #endregion
+
+    #region Placement
+    [TestMethod]
+    [TestCategory("Spacer")]
+    public void Placement_Getter()
+    {
+        // Arrange
+        var spacer = new HeightSpacer();
+
+        // Act
+        var placement = spacer.Placement;
+
+        // Assert
+        Assert.AreEqual(Placement.TopCenter, placement);
+    }
+
+    [TestMethod]
+    [TestCategory("Spacer")]
+    public void UpdatePlacement()
+    {
+        // Arrange
+        var spacer = new HeightSpacer();
+
+        // Act
+        spacer.UpdatePlacement(Placement.TopLeft);
+
+        // Assert
+        Assert.AreEqual(Placement.TopLeft, spacer.Placement);
+    }
+    #endregion
+
+    #region Height
+    [TestMethod]
+    [TestCategory("Spacer")]
+    public void Height_Getter()
+    {
+        // Arrange
+        var spacer = new HeightSpacer(0);
+
+        // Act
+        var height = spacer.Height;
+
+        // Assert
+        Assert.AreEqual(0, height);
+    }
+
+    [TestMethod]
+    [TestCategory("Spacer")]
+    public void Constructor_ThrowsException()
+    {
+        // Act & Assert
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => new HeightSpacer(-1));
+    }
+
+    [TestMethod]
+    [TestCategory("Spacer")]
+    public void Constructor_ThrowsException2()
+    {
+        // Act & Assert
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => new HeightSpacer(int.MaxValue));
+    }
+
+    [TestMethod]
+    [TestCategory("Spacer")]
+    public void UpdateHeight()
+    {
+        // Arrange
+        var spacer = new HeightSpacer(0);
+
+        // Act
+        spacer.UpdateHeight(0);
+
+        // Assert
+        Assert.AreEqual(0, spacer.Height);
+    }
+
+    [TestMethod]
+    [TestCategory("Spacer")]
+    public void UpdateHeight_ThrowsException()
+    {
+        // Arrange
+        var spacer = new HeightSpacer(0);
+
+        // Act & Assert
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => spacer.UpdateHeight(-1));
+    }
+
+    [TestMethod]
+    [TestCategory("Spacer")]
+    public void UpdateHeight_ThrowsException2()
+    {
+        // Arrange
+        var spacer = new HeightSpacer(0);
+
+        // Act & Assert
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => spacer.UpdateHeight(int.MaxValue));
+    }
+    #endregion
+}
