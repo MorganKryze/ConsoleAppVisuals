52fbeef085c4d87ddbab1ea5ab7c08fa556048be
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-03-20 21:23:32 +0000

    📄 add BorderType enum

---
+++ src/ConsoleAppVisuals/enums/BorderType.cs
@@ -0,0 +1,58 @@
+/*
+    GNU GPL License 2024 MorganKryze(Yann Vidamment)
+    For full license information, please visit: https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/LICENSE
+*/
+namespace ConsoleAppVisuals.Enums;
+
+/// <summary>
+/// The <see cref="BorderType"/> enum defines the type of border to use for embed elements.
+/// </summary>
+/// <remarks>
+/// For more information, refer to the following resources:
+/// <list type="bullet">
+/// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+/// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+/// </list>
+/// </remarks>
+public enum BorderType
+{
+    /// <summary>
+    /// Single line borders with straight corners (┌┐└┘─│┬┴├┤┼)
+    /// </summary>
+    /// <remarks>
+    /// Universal compatibility.
+    /// </remarks>
+    SingleStraight,
+
+    /// <summary>
+    /// Single line borders with rounded corners (╭╮╰╯─│┬┴├┤┼)
+    /// </summary>
+    /// <remarks>
+    /// Not supported on PowerShell (Windows).
+    /// </remarks>
+    SingleRounded,
+
+    /// <summary>
+    /// Single line borders with bold lines (┏┓┗┛━┃┳┻┣┫╋)
+    /// </summary>
+    /// <remarks>
+    /// Not supported on PowerShell (Windows).
+    /// </remarks>
+    SingleBold,
+
+    /// <summary>
+    /// Double line borders with straight corners (╔╗╚╝═║╦╩╠╣╬)
+    /// </summary>
+    /// <remarks>
+    /// Universal compatibility.
+    /// </remarks>
+    DoubleStraight,
+
+    /// <summary>
+    /// ASCII borders (+-|+++++)
+    /// </summary>
+    /// <remarks>
+    /// Universal compatibility.
+    /// </remarks>
+    ASCII
+}
