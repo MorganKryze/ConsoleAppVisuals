552d0891f4628064103f804e649b741f15eda8fd
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-03-22 11:14:31 +0000

    🌟 add Borders to ElementList

--- src/ConsoleAppVisuals/elements/passive_elements/inspectors/ElementsList.cs
+++ src/ConsoleAppVisuals/elements/passive_elements/inspectors/ElementsList.cs
@@ -20,7 +20,7 @@ public class ElementsList : PassiveElement
     private ElementType _elementsTypeExpected;
     private List<List<string>> _lines;
     private string[] _displayArray;
-    private bool _roundedCorners;
+    private readonly Borders _borders;
     private Placement _placement;
     #endregion
 
@@ -48,12 +48,6 @@ public class ElementsList : PassiveElement
     /// This property returns the headers of the dashboard.
     /// </summary>
     public static List<string> Headers => new() { "Id", "Type", "Project" };
-    private string GetCorners => _roundedCorners ? "╭╮╰╯" : "┌┐└┘";
-
-    /// <summary>
-    /// This property wether the corners of the InteractiveList are rounded.
-    /// </summary>
-    public bool RoundedCorners => _roundedCorners;
 
     /// <summary>
     /// This property returns the lines of the InteractiveList.
@@ -66,8 +60,14 @@ public class ElementsList : PassiveElement
     public override Placement Placement => _placement;
 
     /// <summary>
-    /// This property returns the line to display the InteractiveList on.
+    /// This property returns the borders of the InteractiveList.
     /// </summary>
+    public Borders Borders => _borders;
+
+    /// <summary>
+    /// This property returns the type of borders of the InteractiveList.
+    /// </summary>
+    public BordersType BordersType => _borders.Type;
 
     /// <summary>
     /// This property returns the height of the InteractiveList.
@@ -97,7 +97,7 @@ public class ElementsList : PassiveElement
     /// </summary>
     /// <param name="elementTypeExpected">The type of element expected.</param>
     /// <param name="placement">The placement of the InteractiveList.</param>
-    /// <param name="roundedCorners">If true, the corners of the InteractiveList will be rounded.</param>
+    /// <param name="bordersType">The type of borders of the InteractiveList.</param>
     /// <remarks>
     /// For more information, refer to the following resources:
     /// <list type="bullet">
@@ -108,35 +108,19 @@ public class ElementsList : PassiveElement
     public ElementsList(
         ElementType elementTypeExpected = ElementType.Default,
         Placement placement = Placement.TopCenter,
-        bool roundedCorners = false
+        BordersType bordersType = BordersType.SingleStraight
     )
     {
         _elementsTypeExpected = elementTypeExpected;
         _lines = UpdateLines();
         _placement = placement;
-        _roundedCorners = roundedCorners;
+        _borders = new Borders(bordersType);
         _displayArray = Array.Empty<string>();
         BuildDisplay();
     }
     #endregion
 
     #region Methods
-    /// <summary>
-    /// Toggles the rounded corners of the element.
-    /// </summary>
-    /// <remarks>
-    /// For more information, refer to the following resources:
-    /// <list type="bullet">
-    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
-    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
-    /// </list>
-    /// </remarks>
-    public void SetRoundedCorners(bool rounded = true)
-    {
-        _roundedCorners = rounded;
-        BuildDisplay();
-    }
-
     /// <summary>
     /// This method updates the placement of the InteractiveList.
     /// </summary>
@@ -151,6 +135,23 @@ public class ElementsList : PassiveElement
     public void UpdatePlacement(Placement placement)
     {
         _placement = placement;
+        BuildDisplay();
+    }
+
+    /// <summary>
+    /// This method updates the type of borders of the InteractiveList.
+    /// </summary>
+    /// <param name="bordersType">The new type of borders of the InteractiveList.</param>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    public void UpdateBordersType(BordersType bordersType)
+    {
+        _borders.UpdateBordersType(bordersType);
         BuildDisplay();
     }
 
@@ -191,8 +192,12 @@ public class ElementsList : PassiveElement
                         types.AddRange(
                             assembly
                                 .GetTypes()
-                                .Where(t => t.BaseType != null && t.IsSubclassOf(typeof(Element))&& t != typeof(PassiveElement) 
-                        && t != typeof(InteractiveElement<>))
+                                .Where(t =>
+                                    t.BaseType != null
+                                    && t.IsSubclassOf(typeof(Element))
+                                    && t != typeof(PassiveElement)
+                                    && t != typeof(InteractiveElement<>)
+                                )
                         );
                         break;
 
@@ -264,60 +269,68 @@ public class ElementsList : PassiveElement
             }
         }
 
-        StringBuilder headerBuilder = new("│ ");
+        StringBuilder headerBuilder = new($"{Borders.Vertical} ");
         for (int i = 0; i < Headers.Count; i++)
         {
             headerBuilder.Append(Headers[i]?.PadRight(localMax[i]) ?? "");
             if (i != Headers.Count - 1)
             {
-                headerBuilder.Append(" │ ");
+                headerBuilder.Append($" {Borders.Vertical} ");
             }
             else
             {
-                headerBuilder.Append(" │");
+                headerBuilder.Append($" {Borders.Vertical}");
             }
         }
         stringList.Add(headerBuilder.ToString());
 
-        StringBuilder upperBorderBuilder = new(GetCorners[0].ToString());
+        StringBuilder upperBorderBuilder = new(Borders.TopLeft.ToString());
         for (int i = 0; i < Headers.Count; i++)
         {
-            upperBorderBuilder.Append(new string('─', localMax[i] + 2));
-            upperBorderBuilder.Append((i != Headers.Count - 1) ? "┬" : GetCorners[1].ToString());
+            upperBorderBuilder.Append(new string(Borders.Horizontal, localMax[i] + 2));
+            upperBorderBuilder.Append(
+                (i != Headers.Count - 1) ? Borders.Top.ToString() : Borders.TopRight.ToString()
+            );
         }
         stringList.Insert(0, upperBorderBuilder.ToString());
 
-        StringBuilder intermediateBorderBuilder = new("├");
+        StringBuilder intermediateBorderBuilder = new(Borders.Left.ToString());
         for (int i = 0; i < Headers.Count; i++)
         {
-            intermediateBorderBuilder.Append(new string('─', localMax[i] + 2));
-            intermediateBorderBuilder.Append((i != Headers.Count - 1) ? "┼" : "┤");
+            intermediateBorderBuilder.Append(new string(Borders.Horizontal, localMax[i] + 2));
+            intermediateBorderBuilder.Append(
+                (i != Headers.Count - 1) ? Borders.Cross.ToString() : Borders.Right.ToString()
+            );
         }
         stringList.Add(intermediateBorderBuilder.ToString());
 
         for (int i = 0; i < _lines.Count; i++)
         {
-            StringBuilder lineBuilder = new("│ ");
+            StringBuilder lineBuilder = new($"{Borders.Vertical} ");
             for (int j = 0; j < _lines[i].Count; j++)
             {
                 lineBuilder.Append(_lines[i][j]?.ToString()?.PadRight(localMax[j]) ?? "");
                 if (j != _lines[i].Count - 1)
                 {
-                    lineBuilder.Append(" │ ");
+                    lineBuilder.Append($" {Borders.Vertical} ");
                 }
                 else
                 {
-                    lineBuilder.Append(" │");
+                    lineBuilder.Append($" {Borders.Vertical}");
                 }
             }
             stringList.Add(lineBuilder.ToString());
         }
 
-        StringBuilder lowerBorderBuilder = new(GetCorners[2].ToString());
+        StringBuilder lowerBorderBuilder = new(Borders.BottomLeft.ToString());
         for (int i = 0; i < Headers.Count; i++)
         {
-            lowerBorderBuilder.Append(new string('─', localMax[i] + 2));
-            lowerBorderBuilder.Append((i != Headers.Count - 1) ? "┴" : GetCorners[3].ToString());
+            lowerBorderBuilder.Append(new string(Borders.Horizontal, localMax[i] + 2));
+            lowerBorderBuilder.Append(
+                (i != Headers.Count - 1)
+                    ? Borders.Bottom.ToString()
+                    : Borders.BottomRight.ToString()
+            );
         }
         stringList.Add(lowerBorderBuilder.ToString());
 
@@ -329,16 +342,16 @@ public class ElementsList : PassiveElement
     {
         var len = _displayArray![0].Length;
         var title = Title.ResizeString(len - 4);
-        title = $"│ {title} │";
-        var upperBorderBuilder = new StringBuilder(GetCorners[0].ToString());
-        upperBorderBuilder.Append(new string('─', len - 2));
-        upperBorderBuilder.Append(GetCorners[1].ToString());
+        title = $"{Borders.Vertical} {title} {Borders.Vertical}";
+        var upperBorderBuilder = new StringBuilder(Borders.TopLeft.ToString());
+        upperBorderBuilder.Append(new string(Borders.Horizontal, len - 2));
+        upperBorderBuilder.Append(Borders.TopRight.ToString());
         var display = _displayArray.ToList();
         display[0] = display[0]
             .Remove(0, 1)
-            .Insert(0, "├")
+            .Insert(0, Borders.Left.ToString())
             .Remove(display[1].Length - 1, 1)
-            .Insert(display[1].Length - 1, "┤");
+            .Insert(display[1].Length - 1, Borders.Right.ToString());
         display.Insert(0, title);
         display.Insert(0, upperBorderBuilder.ToString());
         _displayArray = display.ToArray();
