41de37b11ddc74b1c7c571817603779caa4d6260
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-03-25 20:59:35 +0000

    🌟 work in progress, new dialog element

---
+++ src/ConsoleAppVisuals/elements/interactive_elements/Dialog.cs
@@ -0,0 +1,417 @@
+/*
+    GNU GPL License 2024 MorganKryze(Yann Vidamment)
+    For full license information, please visit: https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/LICENSE
+*/
+namespace ConsoleAppVisuals.InteractiveElements;
+
+/// <summary>
+/// A <see cref="Dialog"/> is an interactive element that displays a dialog bow with one or two choices.
+/// </summary>
+/// <remarks>
+/// For more information, refer to the following resources:
+/// <list type="bullet">
+/// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+/// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+/// </list>
+/// </remarks>
+public class Dialog : InteractiveElement<int>
+{
+    #region Fields
+    private List<string> _lines;
+    private string? _leftOption;
+    private string? _rightOption;
+    private TextAlignment _align;
+    private Placement _placement;
+    private readonly Borders _borders;
+    private List<string>? _textToDisplay;
+    #endregion
+
+    #region Constants
+    private const int EMBED_MARGIN = 2;
+
+    /// <summary>
+    /// The width ratio between the two options of the Dialog.
+    /// It ensures that the options are not too close to each other.
+    /// </summary>
+    private const double WIDTH_RATIO = 1.1;
+    #endregion
+
+    #region Properties
+    /// <summary>
+    /// The position of the Dialog.
+    /// </summary>
+    public override Placement Placement => _placement;
+
+    /// <summary>
+    /// The alignment of the Dialog.
+    /// </summary>
+    public override TextAlignment TextAlignment => _align;
+
+    /// <summary>
+    /// The height of the Dialog.
+    /// </summary>
+    public override int Height => _textToDisplay!.Count;
+
+    /// <summary>
+    /// The width of the Dialog.
+    /// </summary>
+    public override int Width => _textToDisplay!.Max(s => s.Length);
+
+    /// <summary>
+    /// The rows of the Dialog.
+    /// </summary>
+    public List<string> Lines => _lines;
+
+    /// <summary>
+    /// The text of the left option.
+    /// </summary>
+    public string? LeftOption => _leftOption;
+
+    /// <summary>
+    /// The text of the right option.
+    /// </summary>
+    public string? RightOption => _rightOption;
+
+    /// <summary>
+    /// The borders of the Dialog.
+    /// </summary>
+    public Borders Borders => _borders;
+
+    /// <summary>
+    /// The border type of the selector.
+    /// </summary>
+    public BordersType BordersType => _borders.Type;
+
+    /// <summary>
+    /// The text to display.
+    /// </summary>
+    public List<string>? TextToDisplay => _textToDisplay;
+
+    private int MaxLineLength =>
+        Math.Max(
+            _lines.Max(s => s.Length),
+            (int)((_rightOption?.Length ?? 0) + (_leftOption?.Length ?? 0) * WIDTH_RATIO)
+        );
+    #endregion
+
+    #region Constructor
+    /// <summary>
+    /// A <see cref="Dialog"/> is an interactive element that displays a dialog bow with one or two choices.
+    /// </summary>
+    /// <param name="lines">The text to display.</param>
+    /// <param name="leftOption">The text of the left option. Null for no button.</param>
+    /// <param name="rightOption">The text of the right option. Null for no button.</param>
+    /// <param name="align">The alignment of the Dialog.</param>
+    /// <param name="placement">The placement of the Dialog element.</param>
+    /// <param name="bordersType">The type of border to display.</param>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    public Dialog(
+        List<string> lines,
+        string? leftOption = null,
+        string? rightOption = null,
+        TextAlignment align = TextAlignment.Left,
+        Placement placement = Placement.TopCenter,
+        BordersType bordersType = BordersType.SingleStraight
+    )
+    {
+        _lines = lines;
+        _leftOption = leftOption;
+        _rightOption = rightOption;
+        _align = align;
+        _placement = placement;
+        _borders = new Borders(bordersType);
+        if (CheckIntegrity())
+            Build();
+    }
+    #endregion
+
+    #region Methods
+    private bool CheckIntegrity()
+    {
+        if (_lines.Count == 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// This method updates the text of the first option.
+    /// </summary>
+    /// <param name="text">The new text of the first option.</param>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    public void UpdateRightOption(string? text)
+    {
+        _rightOption = text;
+        Build();
+    }
+
+    /// <summary>
+    /// This method updates the text of the second option.
+    /// </summary>
+    /// <param name="text">The new text of the second option.</param>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    public void UpdateLeftOption(string? text)
+    {
+        _leftOption = text;
+        Build();
+    }
+
+    /// <summary>
+    /// This method updates the text of the Dialog.
+    /// </summary>
+    /// <param name="lines">The new text of the Dialog.</param>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    public void UpdateLines(List<string> lines)
+    {
+        _lines.Clear();
+        _lines = lines;
+        Build();
+    }
+
+    /// <summary>
+    /// This method updates the placement of the Dialog.
+    /// </summary>
+    /// <param name="placement">The new placement of the Dialog.</param>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    public void UpdatePlacement(Placement placement)
+    {
+        _placement = placement;
+    }
+
+    /// <summary>
+    /// This method updates the alignment of the Dialog.
+    /// </summary>
+    /// <param name="align">The new alignment of the Dialog.</param>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    public void UpdateTextAlignment(TextAlignment align)
+    {
+        _align = align;
+        Build();
+    }
+
+    /// <summary>
+    /// This method updates the borders of the Dialog.
+    /// </summary>
+    /// <param name="bordersType">The new border type of the Dialog.</param>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    public void UpdateBordersType(BordersType bordersType)
+    {
+        _borders.UpdateBordersType(bordersType);
+        Build();
+    }
+
+    /// <summary>
+    /// Adds a line to the Dialog.
+    /// </summary>
+    /// <param name="line">The line to add.</param>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    public void AddLine(string line)
+    {
+        _lines.Add(line);
+        Build();
+    }
+
+    /// <summary>
+    /// Inserts a line to the Dialog.
+    /// </summary>
+    /// <param name="line">The line to insert.</param>
+    /// <param name="index">The index where to insert the line.</param>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    public void InsertLine(string line, int index)
+    {
+        _lines.Insert(index, line);
+        Build();
+    }
+
+    /// <summary>
+    /// Removes a line from the Dialog.
+    /// </summary>
+    /// <param name="line">The line to remove.</param>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    public void RemoveLine(string line)
+    {
+        if (!_lines.Contains(line))
+        {
+            throw new ArgumentException("The line is not in the text.");
+        }
+        _lines.Remove(line);
+        Build();
+    }
+
+    /// <summary>
+    /// Removes a line from the Dialog.
+    /// </summary>
+    /// <param name="index">The index of the line to remove.</param>
+    /// <remarks>
+    /// For more information, refer to the following resources:
+    /// <list type="bullet">
+    /// <item><description><a href="https://morgankryze.github.io/ConsoleAppVisuals/">Documentation</a></description></item>
+    /// <item><description><a href="https://github.com/MorganKryze/ConsoleAppVisuals/blob/main/example/">Example Project</a></description></item>
+    /// </list>
+    /// </remarks>
+    public void RemoveLine(int index)
+    {
+        if (index < 0 || index >= _lines.Count)
+        {
+            throw new ArgumentOutOfRangeException("The index is out of range.");
+        }
+        _lines.RemoveAt(index);
+        Build();
+    }
+
+    /// <summary>
+    /// Renders the Dialog.
+    /// </summary>
+    [Visual]
+    protected override void RenderElementActions()
+    {
+        // TODO: UPDATE Code to implement new feature
+        Build();
+        Core.WriteMultiplePositionedLines(
+            false,
+            TextAlignment,
+            Placement,
+            false,
+            Line,
+            _textToDisplay!.ToArray()
+        );
+        Window.Freeze();
+    }
+
+    private void Build()
+    {
+        if (!CheckIntegrity())
+        {
+            throw new InvalidOperationException(
+                "The lines are empty. You must provide at least one line to display in the Dialog"
+            );
+        }
+
+        _textToDisplay = new List<string>();
+
+        _textToDisplay.Add(
+            Borders.TopLeft
+                + new string(Borders.Horizontal, MaxLineLength + EMBED_MARGIN)
+                + Borders.TopRight
+        );
+
+        foreach (var line in _lines)
+        {
+            var lineToDisplay = $"{Borders.Vertical} ";
+            switch (_align)
+            {
+                case TextAlignment.Center:
+                    int totalPadding = MaxLineLength - line.Length;
+                    int padLeft = totalPadding / 2;
+                    lineToDisplay += line.PadLeft(line.Length + padLeft).PadRight(MaxLineLength);
+                    break;
+                case TextAlignment.Left:
+                    lineToDisplay += line.PadRight(MaxLineLength);
+                    break;
+                case TextAlignment.Right:
+                    lineToDisplay += line.PadLeft(MaxLineLength);
+                    break;
+            }
+            lineToDisplay += $" {Borders.Vertical}";
+            _textToDisplay.Add(lineToDisplay);
+        }
+
+        if (_rightOption is not null || _leftOption is not null)
+        {
+            AddOptions();
+        }
+
+        _textToDisplay.Add(
+            Borders.BottomLeft
+                + new string(Borders.Horizontal, MaxLineLength + EMBED_MARGIN)
+                + Borders.BottomRight
+        );
+    }
+
+    private void AddOptions()
+    {
+        _textToDisplay!.Add(
+            $"{Borders.Vertical} " + new string(' ', MaxLineLength) + $" {Borders.Vertical}"
+        );
+
+        string optionLine =
+            $"{Borders.Vertical} " + new string(' ', MaxLineLength) + $" {Borders.Vertical}";
+
+        if (_leftOption is not null)
+        {
+            optionLine = optionLine.Remove(2, _leftOption.Length);
+            optionLine = optionLine.Insert(2, _leftOption);
+        }
+
+        if (_rightOption is not null)
+        {
+            int insertPosition = optionLine.Length - _rightOption.Length - 2;
+            optionLine = optionLine.Remove(insertPosition, _rightOption.Length);
+            optionLine = optionLine.Insert(insertPosition, _rightOption);
+        }
+
+        _textToDisplay.Add(optionLine);
+    }
+    #endregion
+}
