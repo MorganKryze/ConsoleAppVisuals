d8c3c4194c8151e8dc2a10ec9028bb4654017cce
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-03-31 20:06:12 +0100

    🚑 solve footer rendering issue margin

--- src/ConsoleAppVisuals/elements/passive_elements/Footer.cs
+++ src/ConsoleAppVisuals/elements/passive_elements/Footer.cs
@@ -17,20 +17,18 @@ public class Footer : PassiveElement
     const string DEFAULT_FOOTER_LEFT = "Footer Left";
     const string DEFAULT_FOOTER_CENTER = "Footer Center";
     const string DEFAULT_FOOTER_RIGHT = "Footer Right";
-    const int DEFAULT_MARGIN = 1;
     const Placement DEFAULT_PLACEMENT = Placement.BottomCenterFullWidth;
     #endregion
 
     #region Fields
     private (string, string, string) _text;
-    private int _upperMargin;
     #endregion
 
     #region Default Properties
     /// <summary>
     /// Gets the height of the footer.
     /// </summary>
-    public override int Height => UpperMargin + TEXT_HEIGHT;
+    public override int Height => TEXT_HEIGHT;
 
     /// <summary>
     /// Gets the width of the footer.
@@ -50,11 +48,6 @@ public class Footer : PassiveElement
     public (string, string, string) Text => _text;
 
     /// <summary>
-    /// Gets the upper margin of the footer.
-    /// </summary>
-    public int UpperMargin => _upperMargin;
-
-    /// <summary>
     /// Gets the maximum number of this element.
     /// </summary>
     public override int MaxNumberOfThisElement => 1;
@@ -67,21 +60,18 @@ public class Footer : PassiveElement
     /// <param name="leftText">The text on the left of the footer.</param>
     /// <param name="centerText">The text in the center of the footer.</param>
     /// <param name="rightText">The text on the right of the footer.</param>
-    /// <param name="upperMargin">The upper margin of the footer.</param>
     /// <remarks>
     /// For more information, consider visiting the documentation available <a href="https://morgankryze.github.io/ConsoleAppVisuals/">here</a>.
     /// </remarks>
     public Footer(
         string leftText = DEFAULT_FOOTER_LEFT,
         string centerText = DEFAULT_FOOTER_CENTER,
-        string rightText = DEFAULT_FOOTER_RIGHT,
-        int upperMargin = DEFAULT_MARGIN
+        string rightText = DEFAULT_FOOTER_RIGHT
     )
     {
         _text.Item1 = leftText;
         _text.Item2 = centerText;
         _text.Item3 = rightText;
-        _upperMargin = upperMargin;
     }
     #endregion
 
@@ -121,27 +111,6 @@ public class Footer : PassiveElement
     {
         _text.Item3 = rightText;
     }
-
-    /// <summary>
-    /// Updates the upper margin of the footer.
-    /// </summary>
-    /// <param name="upperMargin">The new upper margin of the banner.</param>
-    /// <exception cref="ArgumentOutOfRangeException">The upper margin of the banner must be between 0 and the height of the console window.</exception>
-    /// <remarks>
-    /// For more information, consider visiting the documentation available <a href="https://morgankryze.github.io/ConsoleAppVisuals/">here</a>.
-    /// </remarks>
-    public void UpdateUpperMargin(int upperMargin)
-    {
-        var maxWidth = Console.WindowHeight == 0 ? 0 : Console.WindowHeight - 1;
-        if (upperMargin < 0 || upperMargin > maxWidth)
-        {
-            throw new ArgumentOutOfRangeException(
-                nameof(upperMargin),
-                "The upper margin of the banner must be between 0 and the height of the console window."
-            );
-        }
-        _upperMargin = upperMargin;
-    }
     #endregion
 
     #region Rendering
@@ -151,10 +120,6 @@ public class Footer : PassiveElement
     [Visual]
     protected override void RenderElementActions()
     {
-        for (int i = 0; i < UpperMargin; i++)
-        {
-            Core.WritePositionedString(string.Empty, Placement, true, Line + i, false);
-        }
         Core.WritePositionedString(_text.BannerToString(), Placement, true, Line, false);
     }
     #endregion
